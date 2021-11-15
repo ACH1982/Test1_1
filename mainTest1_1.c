c3bcc5ae4b13ae5b141c1281c47dc2e5bec13724
ACHM82 <alvaroc>
2021-11-15 12:13:03 +0100

    First commit in this branch with mainTest1_1.c only

---
+++ mainTest1_1.c
@@ -0,0 +1,18 @@
+// Method calling the functionallity
+#include "printTest1_1.h"
+#include <stdio.h>
+
+int main()
+{
+    // Call the file for printing a sentence
+    printf("\n");
+    printingSentence();
+    printf("\n");
+    printf("Additionally this is printed from main\n");
+    printf("\n");
+    printf("This line, in %s, in the code is numbered by %d", __FILE__, __LINE__);
+    printf("\n");
+    printf("\n");
+
+    return 0;
+}
